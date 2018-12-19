39feaa8b0595019daada9243e3d2d451ea09c122
tatsuya_akashi <tatsuya_akashi@dwango.co.jp>
2018-12-19 19:46:47 +0900

    情報は情報が出るように

--- resources/views/line/tasks.blade.php
+++ resources/views/line/tasks.blade.php
@@ -14,28 +14,47 @@
     @endif
 
     @if(count($tasksGroupedDate) > 0)
-        <table>
-            <tr>
-                <th>日付</th>
-                <th>やったこと</th>
-                <th>やること</th>
-            </tr>
-            @foreach($tasksGroupedDate as $date => $tasks)
+        @if($line['type'] == 'information')
+            <table>
                 <tr>
-                    <td>{{$date}}</td>
-                    <td>
-                        @if(isset($tasks['did']))
-                            <pre>{{$tasks['did']}}</pre>
-                        @endif
-                    </td>
-                    <td>
-                        @if(isset($tasks['do']))
-                            <pre>{{$tasks['do']}}</pre>
-                        @endif
-                    </td>
+                    <th>日付</th>
+                    <th>情報</th>
                 </tr>
-            @endforeach
-        </table>
+                @foreach($tasksGroupedDate as $date => $tasks)
+                    <tr>
+                        <td>{{$date}}</td>
+                        <td>
+                            @if(isset($tasks['information']))
+                                <pre>{{$tasks['information']}}</pre>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        @elseif($line['type'] == 'progress' && isset($tasksGroupedLine[$line['id']]))
+            <table>
+                <tr>
+                    <th>日付</th>
+                    <th>やったこと</th>
+                    <th>やること</th>
+                </tr>
+                @foreach($tasksGroupedDate as $date => $tasks)
+                    <tr>
+                        <td>{{$date}}</td>
+                        <td>
+                            @if(isset($tasks['did']))
+                                <pre>{{$tasks['did']}}</pre>
+                            @endif
+                        </td>
+                        <td>
+                            @if(isset($tasks['do']))
+                                <pre>{{$tasks['do']}}</pre>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        @endif
     @else
         <p>empty</p>
     @endif
